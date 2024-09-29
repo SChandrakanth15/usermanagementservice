93759400de385a13e8e6527d24be671df626d563
SChandrakanth15 <schandrakanth15@gmail.com>
2024-09-29 23:21:14 +0530

    changed the url to docker container url

--- src/main/java/com/theelixrlabs/UserManagementService/config/SecurityConfig.java
+++ src/main/java/com/theelixrlabs/UserManagementService/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500")); // Frontend origin
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500","http://localhost","http://frontend-image")); // Frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
--- src/main/java/com/theelixrlabs/UserManagementService/filter/JwtTokenFilter.java
+++ src/main/java/com/theelixrlabs/UserManagementService/filter/JwtTokenFilter.java
@@ -25,7 +25,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private String currentToken;
 
     public JwtTokenFilter(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://localhost:8081").build();
+        this.webClient = webClientBuilder.baseUrl("http://authservice:8081").build();
     }
 
     @Override
