6385fe41f286c3b7dd0c041df8e70ad7b03c5059
SChandrakanth15 <schandrakanth15@gmail.com>
2024-10-01 15:04:57 +0530

    constants added successfully and need to add the logging statements

---
+++ src/main/java/com/theelixrlabs/UserManagementService/constants/ApiPathsConstant.java
@@ -0,0 +1,10 @@
+package com.theelixrlabs.UserManagementService.constants;
+
+public class ApiPathsConstant {
+    public static final String USERS_BASE_ENDPOINT = "/users";
+    public static final String REGISTER_A_USER_ENDPOINT = "/register";
+    public static final String GET_ALL_USERS_EXCEPT_ENDPOINT = "/except";
+    public static final String UPDATE_A_USER_ENDPOINT = "/update";
+    public static final String DELETE_A_USER_ENDPOINT = "/delete";
+
+}
